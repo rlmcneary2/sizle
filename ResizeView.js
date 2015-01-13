34940f6da799391882fdaac00d09efd7db166fa5
Rich McNeary <rlm2public@gmail.com>
2015-01-12 20:05:15 -0500

    Fix project name in license text.

--- ResizeView.js
+++ ResizeView.js
@@ -3,20 +3,20 @@
 /// <reference path="ResourceCreator.js" />
 /// <reference path="ResizeModel.js" />
 
-// This file is part of PS Resize.
+// This file is part of sizle.
 
-// PS Resize is free software: you can redistribute it and/or modify
+// sizle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// PS Resize is distributed in the hope that it will be useful,
+// sizle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with PS Resize.  If not, see <http://www.gnu.org/licenses/>.
+// along with sizle.  If not, see <http://www.gnu.org/licenses/>.
 
 (function () {
 
